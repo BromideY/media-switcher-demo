f29da329e02da09ba1eb61135f56a894a878b395
Jason <yanghao19921105@outlook.com>
2024-05-21 16:27:02 +0800

    temp commit

--- Start.sh
+++ Start.sh
@@ -4,7 +4,8 @@ cd srs && ./srs -c srs.conf
 
 cd ..
 
-export LD_LIBRARY_PATH=./lib
+export LD_LIBRARY_PATH=`pwd`/lib:`pwd`/lib/cef
+export LD_PRELOAD=`pwd`/lib/cef/libcef.so
 
 export HTTP_SERVER_LISTEN_PORT=10001
 
